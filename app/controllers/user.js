7f224a1463f7a4d74b2e554fd4010cb24286c98e
jerry valsin <jerryvalsin@gmail.com>
2020-05-09 18:08:01 +0200

    first commit

---
+++ app/controllers/user.js
@@ -0,0 +1,193 @@
+const UserModel = require('../models/user.js')
+
+/**
+ * User
+ * @class
+ */
+class User {
+  constructor (app, connect) {
+    this.app = app
+    this.UserModel = connect.model('User', UserModel)
+
+    this.createUser()
+    this.showUsers()
+    this.showUser()
+    this.searchUser()
+    this.updateUser()
+    this.deleteUser()
+    this.deleteUsers()
+  }
+
+  /**
+   * Create one user
+   */
+  createUser () {
+    this.app.post('/user/create', (req, res) => {
+      try {
+        const userModel = this.UserModel(req.body)
+
+        userModel
+          .save()
+          .then((user) => {
+            res.status(200).json(user || {})
+          })
+          .catch((err) => {
+            res.status(500).json({
+              code: 500,
+              message: err
+            })
+          })
+      } catch (err) {
+        res.status(500).json({
+          code: 500,
+          message: err
+        })
+      }
+    })
+  }
+
+  /**
+   * Show All
+   */
+  showUsers () {
+    this.app.get('/user/show', (req, res) => {
+      try {
+        this.UserModel.find({}).then((user) => {
+          res.status(200).json(user)
+        })
+      } catch (err) {
+        res.status(500).json({
+          code: 500,
+          message: err
+        })
+      }
+    })
+  }
+
+  /**
+   * Show one by id
+   */
+  showUser () {
+    this.app.get('/user/show/:id', (req, res) => {
+      try {
+        this.UserModel.findById(req.params.id)
+          .then((user) => {
+            res.status(200).json(user || {})
+          })
+          .catch((err) => {
+            res.status(500).json({
+              code: 500,
+              message: err
+            })
+          })
+      } catch (err) {
+        res.status(500).json({
+          code: 500,
+          message: err
+        })
+      }
+    })
+  }
+
+  /**
+   * List user limit 10
+   */
+  searchUser () {
+    this.app.post('/user/search', (req, res) => {
+      try {
+        const pipe = [{ $limit: req.body.limit || 10 }]
+
+        if (req.body.sort) {
+          pipe.push({ $sort: req.body.sort })
+        }
+
+        this.UserModel.aggregate(pipe)
+          .then((user) => {
+            res.status(200).json(user || {})
+          })
+          .catch((err) => {
+            res.status(500).json({
+              code: 500,
+              message: err
+            })
+          })
+      } catch (err) {
+        res.status(500).json({
+          code: 500,
+          message: err
+        })
+      }
+    })
+  }
+
+  /**
+   * Update one user
+   */
+  updateUser () {
+    this.app.put('/user/update/:id', (req, res) => {
+      try {
+        this.UserModel.findByIdAndUpdate(req.params.id, req.body).then(user => {
+          res.status(200).json(user || {})
+        }).catch(err => {
+          res.status(500).json({
+            code: 500,
+            message: err
+          })
+        })
+      } catch (err) {
+        res.status(500).json({
+          code: 500,
+          message: err
+        })
+      }
+    })
+  }
+
+  /**
+   * Delete one user
+   */
+  deleteUser () {
+    this.app.delete('/user/delete/:id', (req, res) => {
+      try {
+        this.UserModel.findByIdAndDelete(req.params.id).then(user => {
+          res.status(200).json(user || {})
+        }).catch(err => {
+          res.status(500).json({
+            code: 500,
+            message: err
+          })
+        })
+      } catch (err) {
+        res.status(500).json({
+          code: 500,
+          message: err
+        })
+      }
+    })
+  }
+  
+  /**
+   * Delete all
+   */
+  deleteUsers () {
+    this.app.delete('/user/delete', (req, res) => {
+      try {
+        this.UserModel.remove({}).then(user => {
+          res.status(200).json(user)
+        }).catch(err => {
+          res.status(500).json({
+            code: 500,
+            message: err
+          })
+        })
+      } catch (err) {
+        res.status(500).json({
+          code: 500,
+          message: err
+        })
+      }
+    })
+  }
+}
+
+module.exports = User
---
+++ app/models/user.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose')
+
+const Schema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  last_name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    trim: true
+  },
+  street_number: {
+    type: String,
+    trim: true
+  },
+  street_type: {
+    type: String,
+    trim: true
+  },
+  street_name: {
+    type: String,
+    trim: true
+  },
+  city: {
+    type: String,
+    trim: true
+  },
+  city_code: {
+    type: String,
+    trim: true
+  },
+  phone: {
+    type: String
+  },
+  image_profil: {
+    type: String,
+    default: 'https://pbs.twimg.com/profile_images/1126137112825335808/L5WvNz8W_400x400.jpg'
+  },
+  creation_date: {
+    type: Date,
+    default: Date.now
+  },
+  updated_date: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  collection: 'users',
+  minimize: false,
+  versionKey: false
+}).set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id
+
+    delete ret._id
+  }
+})
+
+module.exports = Schema
